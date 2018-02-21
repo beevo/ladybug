d56df62dfcaabdf75587c3b8c041154435e8e1f1
Bee Vo <vo.bee92@gmail.com>
2018-02-21 19:34:34 +0000

    Add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- CSRF Token -->
 		<meta name="csrf-token" content="{{ csrf_token() }}">
-		<title>{{ config('app.name', 'Laravel') }}</title>
+		<title>{{ config('app.name') }}</title>
 		<!-- Styles -->
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.4/css/materialize.min.css">
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -38,6 +38,40 @@
 			<div class="container">
 				@yield('content')
 			</div>
+			<footer class="page-footer">
+				<div class="container">
+					<div class="row">
+						<div class="col l6 s12">
+							<h5 class="white-text">Footer Content</h5>
+							<p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+						</div>
+						<div class="col l4 offset-l2 s12">
+							<h5 class="white-text">Links</h5>
+							<ul>
+								<li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+								{{-- <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+								<li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+								<li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li> --}}
+							</ul>
+						</div>
+					</div>
+				</div>
+
+				<div class="footer-copyright">
+
+					<div class="container">
+
+						© 2014 Copyright Text
+
+						<a class="grey-text text-lighten-4 right" href="#!">
+							By Bee Vo
+						</a>
+
+					</div>
+
+				</div>
+
+			</footer>
 		</div>
 		<!-- Scripts -->
 		<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
